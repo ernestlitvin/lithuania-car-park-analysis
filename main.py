5cc171b966ef7ea4c3df6b5ecd99976f86098b24
Ernest Litvin <erik.litvin@gmail.com>
2025-10-21 10:07:15 +0300

    Data Cleaning & Transformation

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+# # =============================================================================
+# # === 1. LIBRARY IMPORTS ===
+# # =============================================================================
 import pandas as pd
-
-# 1. Cols to use
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+# =============================================================================
+# === 2. DATA LOADING & PREPARATION ===
+# =============================================================================
+# Load the dataset
+# 1.1. Columns to use
 cols_to_use = [
     'MARKE',
     'KOMERCINIS_PAV',
@@ -11,8 +20,7 @@ cols_to_use = [
     'GALIA',
     'SAVIVALDYBE'
 ]
-
-# 2. Dictionary for optimization of data types. We use 'category' to save memory.
+# 1.2. Dictionary for optimization of data types. Using "category" to save memory, since the main file is over 900 MB
 data_types = {
     'MARKE': 'category',
     'KOMERCINIS_PAV': 'category',
@@ -20,17 +28,42 @@ data_types = {
     'SPALVA': 'category',
     'SAVIVALDYBE': 'category'
 }
-
 file_path = "Atviri_TP_parko_duomenys.csv"
-
 df_cars = pd.read_csv(
     file_path,
     usecols = cols_to_use,
     dtype=data_types
 )
+# --- Data Cleaning & Transformation ---
+# Renaming columns to EN language ---
 
-# print("Data successfully loaded!")
-# df_cars.info()
+lt_cols = {
+    'MARKE': 'mark',
+    'KOMERCINIS_PAV': 'model',
+    'DEGALAI': 'fuel_type',
+    'GAMYBOS_METAI': 'production_year',
+    'PIRM_REG_DATA': 'first_reg_date',
+    'SPALVA': 'color',
+    'GALIA': 'power',
+    'SAVIVALDYBE': 'municipality'
+}
+df_cars = df_cars.rename(columns=lt_cols)
+
+# Converting column "date" to datetime-format
+df_cars["first_reg_date"] = pd.to_datetime(df_cars["first_reg_date"])
+
+df_cars.info()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
